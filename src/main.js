05a8ae3731ce9d4662d475e604086dcb8dc75de0
lucas <lucasbomfimsobral@gmail.com>
2024-12-11 22:18:15 -0300

    feat: Adicionar seção footer

--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
 import { cities } from './cities.cards.js';
 import { products } from './products.list.js';
+import { plans } from './plans.cards.js';
 
 const cardsContainer = document.getElementById('cards-container');
-const productsContainer = document.querySelector('.product__container'); // Corrigido para querySelector
+const productsContainer = document.querySelector('.product__container');
+const plansContainer = document.querySelector('.plan__container');
 
-// Renderizando os cartões de cidades
 cities.forEach(city => {
     const card = document.createElement('div');
     card.className = 'card';
-
     card.innerHTML = `
         <div class="card__image"><img src="${city.image}" alt="${city.title}"></div>
         <h3 class="card__title">${city.title}</h3>
         <p class="card__description">${city.description}</p>
     `;
-
     cardsContainer.appendChild(card);
 });
 
-// Renderizando os produtos
 products.forEach(product => {
     const productDiv = document.createElement('div');
-
     productDiv.className = 'product__list';
     productDiv.innerHTML = `<div class="product__card">${product.text}</div>`;
     productsContainer.appendChild(productDiv);
+});
+
+plans.forEach(plan => {
+    const planDiv = document.createElement('div');
+    planDiv.className = 'plan__list';
+    planDiv.innerHTML = `
+        <div class="plan__card">
+            <h2 class="plan__card-title">${plan.title}</h2>
+            <p class="plan__card-phase">${plan.phase}</p>
+            <button class="plan__card-button">Lorem ipsum</button>                     
+        </div>
+    `;
+    plansContainer.appendChild(planDiv);
 });
---
+++ src/plans.cards.js
@@ -0,0 +1,17 @@
+export const plans = [
+    {
+        id: 0,
+        title: 'Lorem ipsum dolor sit amet',
+        phase: 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.',
+    },
+    {
+        id: 1,
+        title: 'Lorem ipsum dolor sit amet',
+        phase: 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.',
+    },
+    {
+        id: 2,
+        title: 'Lorem ipsum dolor sit amet',
+        phase: 'Lorem ipsum dolor sit amet consectetur. Semper orci adipiscing faucibus sit scelerisque quis commodo aenean viverra.',
+    },
+];
